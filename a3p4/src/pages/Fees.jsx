c0324fe4dc61ef86aa5166c234d68f46b3e06cce
sanjeev-yadavv <kumarsanjeev.sk575@gmail.com>
2024-06-13 18:52:22 +0530

    complete assignment 3

---
+++ a3p4/src/pages/Fees.jsx
@@ -0,0 +1,10 @@
+
+export default function Fees () {
+    return (
+        <div>
+            <h1>
+                Fees Page
+            </h1>
+        </div>
+    )
+}
---
+++ a3p4/src/pages/Learn.jsx
@@ -0,0 +1,10 @@
+
+export default function Learn () {
+    return (
+        <div>
+            <h1>
+                Learn Page
+            </h1>
+        </div>
+    )
+}
